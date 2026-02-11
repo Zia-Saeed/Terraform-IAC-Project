44b97c678617c623549fd3e4333cd50e51e478b1
Ziauddin Saeed <117837460+Zia-Saeed@users.noreply.github.com>
2026-02-11 10:24:48 -0800

    Update main.tf

--- terraform-alb-ec2-server-vpc/main.tf
+++ terraform-alb-ec2-server-vpc/main.tf
@@ -1,4 +1,4 @@
-#
+# AWS VPC creation
 resource "aws_vpc" "vpc_1" {
   cidr_block = "192.168.0.0/16"
   region = "us-east-1"
@@ -8,7 +8,7 @@ resource "aws_vpc" "vpc_1" {
     Project = var.project_name
   }
 }
-#
+# AWS internet Gateway Creation
 resource "aws_internet_gateway" "ig_1" {
   vpc_id = aws_vpc.vpc_1.id
   tags = {
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "ig_1" {
     Project = var.resouce_name
   }
 }
-#
+# VPC public subnet 1
 resource "aws_subnet" "pub_sub_1" {
   vpc_id = aws_vpc.vpc_1.id
   cidr_block = var.cidr_block[0]
@@ -28,7 +28,7 @@ resource "aws_subnet" "pub_sub_1" {
     Project = var.project_name
   }
 }
-#
+# VPC public subnet 2
 resource "aws_subnet" "pub_sub_2" {
   cidr_block = var.cidr_block[1]
   vpc_id = aws_vpc.vpc_1.id
@@ -39,7 +39,7 @@ resource "aws_subnet" "pub_sub_2" {
     Environment = var.environment_name
   }
 }
-#
+# VPC public subnet 3 
 resource "aws_subnet" "pub_sub_3" {
   cidr_block = var.cidr_block[2]
   vpc_id = aws_vpc.vpc_1.id
@@ -50,7 +50,7 @@ resource "aws_subnet" "pub_sub_3" {
     Environment = var.environment_name
   }
 }
-#
+# AWS route table for public subnets
 resource "aws_route_table" "pub_route_1" {
   vpc_id = aws_vpc.vpc_1.id
   route {
@@ -63,22 +63,22 @@ resource "aws_route_table" "pub_route_1" {
     Environment = var.environment_name
   }
 }
-#
+# Public subnet 1 association with public route table
 resource "aws_route_table_association" "associate_pub_1" {
   route_table_id = aws_route_table.pub_route_1.id
   subnet_id = aws_subnet.pub_sub_1.id
 }
-#
+# Public subnet 2 association with public route table 
 resource "aws_route_table_association" "associate_pub_2" {
   route_table_id = aws_route_table.pub_route_1.id
   subnet_id = aws_subnet.pub_sub_2.id
 }
-#
+# Public subnet 3 association with public route table
 resource "aws_route_table_association" "associate_pub_3" {
   route_table_id = aws_route_table.pub_route_1.id
   subnet_id = aws_subnet.pub_sub_3.id
 }
-#
+# VPC private subnet 1 creation
 resource "aws_subnet" "pri_sub_1" {
   vpc_id = aws_vpc.vpc_1.id
   cidr_block = var.cidr_block[3]
@@ -89,7 +89,7 @@ resource "aws_subnet" "pri_sub_1" {
     Environment = var.environment_name
   }
 }
-#
+# VCP private subnet 2 creation
 resource "aws_subnet" "pri_sub_2" {
   vpc_id = aws_vpc.vpc_1.id
   cidr_block = var.cidr_block[4]
@@ -100,7 +100,7 @@ resource "aws_subnet" "pri_sub_2" {
     Environment = var.environment_name
   }
 }
-#
+# VPC private subnet 3 creation
 resource "aws_subnet" "pri_sub_3" {
   vpc_id = aws_vpc.vpc_1.id
   cidr_block = var.cidr_block[5]
@@ -111,7 +111,7 @@ resource "aws_subnet" "pri_sub_3" {
     Environment = var.environment_name
   }
 }
-#
+# Elastic IP address for nategateway
 resource "aws_eip" "eip_1" {
   domain = "vpc"
   tags = {
@@ -120,7 +120,7 @@ resource "aws_eip" "eip_1" {
     Environment = var.environment_name
   }
 }
-#
+# Natgateway for Private subnets to access internet
 resource "aws_nat_gateway" "nat_1" {
   subnet_id = aws_subnet.pub_sub_1.id
   allocation_id = aws_eip.eip_1.id
@@ -131,7 +131,7 @@ resource "aws_nat_gateway" "nat_1" {
     Environment = var.environment_name
   }
 }
-#
+# Route table for private subnets
 resource "aws_route_table" "pir_route_1" {
   vpc_id = aws_vpc.vpc_1.id
   route {
@@ -144,22 +144,22 @@ resource "aws_route_table" "pir_route_1" {
     Environment = var.environment_name
   }
 }
-#
+# private subnet 1 association with private route table
 resource "aws_route_table_association" "associate_pri_1" {
  route_table_id = aws_route_table.pir_route_1.id
  subnet_id = aws_subnet.pri_sub_1.id
 }
-#
+# private subnet 2 association with private route table
 resource "aws_route_table_association" "associate_pri_2" {
   route_table_id = aws_route_table.pir_route_1.id
   subnet_id = aws_subnet.pri_sub_2.id
 }
-#
+# private subnet 3 association with private route table
 resource "aws_route_table_association" "associate_pri_3" {
   route_table_id = aws_route_table.pir_route_1.id
   subnet_id = aws_subnet.pri_sub_3.id
 }
-#
+# security group for servers
 resource "aws_security_group" "sg_for_servers" {
   vpc_id = aws_vpc.vpc_1.id
   ingress {
@@ -186,12 +186,12 @@ resource "aws_security_group" "sg_for_servers" {
     Environment = var.environment_name
   }
 }
-#
+# EC2 intance 1
 resource "aws_instance" "server_1" {
   ami = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   security_groups = [ aws_security_group.sg_for_servers.id ]
   associate_public_ip_address = true
-  key_name = "corvit-demo-practise"
+  key_name = "<private-key-name>"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.pub_sub_1.id
 user_data_base64 = base64encode(<<EOF
@@ -220,14 +220,14 @@ EOF
     Environment = var.environment_name
   }
 }
-#
+# EC2 intance 2
 resource "aws_instance" "server_2" {
   ami = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   security_groups = [ aws_security_group.sg_for_servers.id ]
   instance_type = "t2.micro"
   associate_public_ip_address = true
   subnet_id = aws_subnet.pub_sub_2.id
-  key_name = "corvit-demo-practise"
+  key_name = "<private-key-name>"
 user_data_base64 = base64encode(<<EOF
 #!/bin/bash
 yum update -y
@@ -253,7 +253,7 @@ EOF
     Environment = var.environment_name
   }
 }
-#
+# Target groups for application laod balancer
 resource "aws_lb_target_group" "al_target_group_1" {
   vpc_id = aws_vpc.vpc_1.id
   port = 80
@@ -268,17 +268,17 @@ resource "aws_lb_target_group" "al_target_group_1" {
     matcher = 200
   }
 }
-#
+# Target association with ALB target group (EC2 server 1)
 resource "aws_lb_target_group_attachment" "tg_server_1" {
   target_group_arn = aws_lb_target_group.al_target_group_1.arn
   target_id = aws_instance.server_1.id
 }
-#
+# Target association with ALB target group (EC2 server 2)
 resource "aws_lb_target_group_attachment" "tg_server_2" {
   target_group_arn = aws_lb_target_group.al_target_group_1.arn
   target_id = aws_instance.server_2.id
 }
-#
+# Security Group for Application Load Balancer
 resource "aws_security_group" "sg_for_alb" {
   vpc_id = aws_vpc.vpc_1.id
   ingress {
@@ -299,7 +299,7 @@ resource "aws_security_group" "sg_for_alb" {
     Environment = var.environment_name
   }
 }
-#
+# Application Load Balancer
 resource "aws_lb" "alb_for_servers" {
   name = "${var.resouce_name}-alb-for-ec2-server"
   subnets = [ aws_subnet.pub_sub_1.id, aws_subnet.pub_sub_2.id ]
@@ -313,7 +313,7 @@ resource "aws_lb" "alb_for_servers" {
     Project = var.project_name
   }
 }
-#
+# Lister for Application Load Balancer
 resource "aws_alb_listener" "alb_listener" {
   port = 80
   protocol = "HTTP"
